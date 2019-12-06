7a35124e8464bda1e0a1506443b57c2651ab6464
djlonboy <djlonboy@gmail.com>
2019-12-06 11:51:25 +0000

    Added project folder + setup thermostat file and spec

---
+++ src/thermostat.js
@@ -0,0 +1,22 @@
+// Thermostat starts at 20 degrees
+// Power saving on/off - on by default
+
+function Thermostat() {
+}
+
+// Increase temp using up function
+
+// Decrease temp using down function
+
+// Min temp 10 degrees
+
+// Max temp:
+  // If power saving off: 25 degrees
+  // If power saving on: 32 degrees
+
+// Reset: resets temp to 20 degrees
+
+// Return energy usage:
+  // less than 18: low usage (green)
+  // less than 25: medium usage (black)
+  // 25 or more: high usage (red)
