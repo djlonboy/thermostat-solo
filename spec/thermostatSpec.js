95938da595a15bf4b36083b8508bb4b18875669b
djlonboy <djlonboy@gmail.com>
2019-12-06 13:23:44 +0000

    Completed TDD for all features of thermostat model

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,3 +1,100 @@
 describe("thermostat", function() {
 
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  describe("new instance", function() {
+
+    it("should have default temp of 20", function() {
+      expect(thermostat.currentTemp).toEqual(20);
+    });
+
+    it("should have powersave enabled by default", function() {
+      expect(thermostat.powerSaving).toBeTrue();
+    });
+  });
+
+  describe("up function", function() {
+
+    it("should increase temp by 1 degree", function() {
+      thermostat.up();
+      expect(thermostat.currentTemp).toEqual(21);
+    });
+
+    it("if temp is 25 and powersave is on, temp should remain 25", function() {
+      thermostat.currentTemp = 25;
+      thermostat.powerSaving = true;
+      thermostat.up();
+      expect(thermostat.currentTemp).toEqual(25);
+    });
+
+    it("if temp is 32 and powersave is off, temp should remain 32", function() {
+      thermostat.currentTemp = 32;
+      thermostat.powerSaving = false;
+      thermostat.up();
+      expect(thermostat.currentTemp).toEqual(32);
+    });
+
+    it("if temp is > 25 and powerSave is on, temp should return to 25", function() {
+      thermostat.currentTemp = 32;
+      thermostat.powerSaving = true;
+      thermostat.up();
+      expect(thermostat.currentTemp).toEqual(25);
+    });
+  });
+
+  describe("down function", function() {
+
+    it("should decrese temp by 1 degree", function() {
+      thermostat.down();
+      expect(thermostat.currentTemp).toEqual(19);
+    });
+
+    it("if temp is 10, temp should remain 10", function() {
+      thermostat.currentTemp = 10;
+      thermostat.down();
+      expect(thermostat.currentTemp).toEqual(10);
+    });
+
+    it("if temp is < 10, temp should return to 10", function() {
+      thermostat.currentTemp = 6;
+      thermostat.down();
+      expect(thermostat.currentTemp).toEqual(10);
+    });
+  });
+
+  describe("reset function", function() {
+
+    it("should reset the temp to the default", function() {
+      thermostat.currentTemp = 28;
+      thermostat.reset();
+      expect(thermostat.currentTemp).toEqual(20);
+      thermostat.currentTemp = 12;
+      thermostat.reset();
+      expect(thermostat.currentTemp).toEqual(20);
+    });
+  });
+
+  describe("energy usage", function() {
+
+    it("should return 'low usage' if temp is < 18", function () {
+      thermostat.currentTemp = 15
+      expect(thermostat.returnUsage()).toEqual("low usage")
+    });
+
+    it("should return 'medium usage' if temp is < 25 and >= 18", function () {
+      thermostat.currentTemp = 23
+      expect(thermostat.returnUsage()).toEqual("medium usage")
+      thermostat.currentTemp = 18
+      expect(thermostat.returnUsage()).toEqual("medium usage")
+    });
+
+    it("should return 'medium usage' if temp is >= 25", function () {
+      thermostat.currentTemp = 29
+      expect(thermostat.returnUsage()).toEqual("high usage")
+      thermostat.currentTemp = 25
+      expect(thermostat.returnUsage()).toEqual("high usage")
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,21 +2,66 @@
 // Power saving on/off - on by default
 
 function Thermostat() {
-}
+  this.defaultTemp = 20;
+  this.powerSavingDefault = true;
+  this.powerSaveMax = 25;
+  this.noPowerSaveMax = 32;
+  this.minTemp = 10;
+
+  this.powerSaving = this.powerSavingDefault;
+  this.currentTemp = this.defaultTemp;
+};
 
 // Increase temp using up function
+  // Max temp:
+    // If power saving off: 25 degrees
+    // If power saving on: 32 degrees
+Thermostat.prototype.up = function() {
+  if (this.powerSaving == true) {
+    if (this.currentTemp >= this.powerSaveMax) {
+      this.currentTemp = this.powerSaveMax;
+    } else {
+      this.currentTemp += 1;
+    }
+  } else {
+    if (this.currentTemp >= this.powerSaveMax) {
+      this.currentTemp = this.noPowerSaveMax;
+    } else {
+      this.currentTemp += 1;
+    }
+  }
+};
 
 // Decrease temp using down function
-
-// Min temp 10 degrees
-
-// Max temp:
-  // If power saving off: 25 degrees
-  // If power saving on: 32 degrees
+  // Min temp 10 degrees
+Thermostat.prototype.down = function() {
+  if (this.currentTemp <= this.minTemp) {
+    this.currentTemp = this.minTemp;
+  } else {
+    this.currentTemp -= 1;
+  }
+};
 
 // Reset: resets temp to 20 degrees
+Thermostat.prototype.reset = function() {
+  this.currentTemp = this.defaultTemp;
+};
 
 // Return energy usage:
+Thermostat.prototype.returnUsage = function() {
+
   // less than 18: low usage (green)
-  // less than 25: medium usage (black)
-  // 25 or more: high usage (red)
+  if (this.currentTemp < 18) {
+    return "low usage";
+
+      // less than 25: medium usage (black)
+  } else if (this.currentTemp < 25 && this.currentTemp >= 18) {
+    return "medium usage";
+
+    // 25 or more: high usage (red)
+  } else if (this.currentTemp >= 25) {
+    return "high usage";
+  } else {
+    throw "Temperature Error"
+  };
+};
